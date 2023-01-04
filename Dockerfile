601402386c0c7846099400e5b8532f575e95e24e
nikita <nik_z12@mail.ru>
2023-01-04 23:27:38 +0300

    redesigning the project for a multi-module project

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM maven:3.8.6-jdk-11
+ADD . /home/workdir/
+WORKDIR /home/workdir/
+RUN mvn install -D maven.test.skip=true
+
+FROM openjdk:11
+
+COPY --from=0 /home/workdir/app/target/imageUI.jar /home/workdir/app.jar
+WORKDIR /home/workdir/
+CMD ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/example/serverrest/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.serverrest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class TestController {
+    @GetMapping
+    public String getTest(){
+        return "Hello";
+    }
+}
